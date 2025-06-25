fbadb1f360d6a4e4d212a1a02c0eee4229ab21a1
Ishpriya-Sharma <ishpriyasharma15@gmail.com>
2025-06-26 03:01:58 +0530

    Update 03_conversionOperation.js

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+//let score = 33 //number
+
+let score = "33" //string
+
+//const {score} = req.body
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
