4443cac3d826fc2ee8c0f774535e11961e007c1f
Ishpriya-Sharma <ishpriyasharma15@gmail.com>
2025-06-26 04:51:42 +0530

    Update: 03_conversionOperation.js

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,6 +1,6 @@
-//let score = 33 //number
+let score = 33 //number
 
-let score = "33" //string
+//let score = "33" //string
 
 //const {score} = req.body
 
@@ -9,3 +9,19 @@ console.log(typeof (score));
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
+console.log(valueInNumber);//NaN Not a number
+
+//let score = null //0
+//let score = undefined //NaN
+//let score = true //1
+
+let isLoggedIn = 1
+//let isLoggedIn = "Ishpriya" //True
+//let isLoggedIn = "" //False
+let boleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(boleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
