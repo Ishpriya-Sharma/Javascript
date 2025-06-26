24b17fd7ba6fdedeb98e490b380fc0ff9048ab11
Ishpriya-Sharma <ishpriyasharma15@gmail.com>
2025-06-26 05:55:52 +0530

    Add dataTypes-summary.js

---
+++ 01_Basics/dataTypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, underfined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsdeTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//const bigNumber = 6258153751682665n
+
+//Refrence (Non Perimitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj ={
+    name: "Ishpriya",
+    age: 19,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+
+console.log(typeof outsdeTemp);
+console.log(typeof scoreValue);
+console.log(typeof myFunction);
